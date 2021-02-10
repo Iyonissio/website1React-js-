4ef77a32e92d90c9418c4d53b58a4c85788df9e7
Iyonissio <iyonissio01@gmail.com>
2021-02-10 11:16:00 -0800

    some changes

---
+++ src/components/HeroElements.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+import Image3 from '../images2/Moz.jpg'
+
+export const HeroContainer = styled.div`
+    background: linear-gradient(to right, rgba(0,0,0,0.7),
+    rgba(0,0,0,0.1)), 
+    url(${Image3});
+    height: 100vh;
+    background-position: center;
+    background-size: cover;
+`;
--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -2,19 +2,23 @@ import React from 'react'
 import '../App.css';
 import {Button} from './Button';
 import './HeroSection.css';
+import { HeroContainer } from './HeroElements';
 
 function HeroSection() {
     return (
-        <div className='hero-container'>
-            <video src='/videos/video-2.mp4' autoPlay loop muted />
-            <h1>TOGGLE BRAINS</h1>
-            <p>O Mundo das Tecnologias Espera por Ti!</p>
-            <div className="hero-btns">
-                <Button className='btns' buttonStyle='btn--outline' buttonSize='btn--large'>INICIAR</Button>
-                <Button className='btns' buttonStyle='btn--primary' buttonSize='btn--large'>
-                ASSISTIR APRESENTACAO<i className='far fa-play-circle'/></Button>
-            </div>
-        </div>
+        <HeroContainer>
+            <div className='hero-container'>  
+                <h1>TOGGLE BRAINS</h1>
+                <p>O Mundo das Tecnologias Espera por Ti!</p>
+                <div className="hero-btns">
+                    <Button className='btns' buttonStyle='btn--outline' buttonSize='btn--large'>INICIAR</Button>
+                    <Button className='btns' buttonStyle='btn--primary' buttonSize='btn--large'>
+                    ASSISTIR APRESENTACAO<i className='far fa-play-circle'/></Button>
+                    </div>
+            </div>  
+        </HeroContainer>
+        
+        
     )
 }
 
